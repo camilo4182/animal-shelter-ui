b9b9d9be8a8b63d54ab90f327276e895290b77da
Johan Camilo Cortes Ocampo <camilo4182@gmail.com>
2021-05-14 00:25:07 -0500

    consumer version added

--- test/contract/publish.js
+++ test/contract/publish.js
@@ -5,6 +5,7 @@ dotenv.config();
 const opts = {
     pactBroker: process.env.PACT_BROKER_BASE_URL,
     pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+    consumerVersion: process.env.PACT_CONSUMER_VERSION,
     pactFilesOrDirs: ['./test/contract/pacts/animalshelterfront-animalshelterback.json']
 };
 
